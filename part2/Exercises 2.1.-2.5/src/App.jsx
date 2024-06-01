6fbbb18935276397405fa69d1b06e24d82e26260
Phillip Radon <phillip.radon@yahoo.com>
2024-06-01 12:09:46 +0200

    startet part2 Exercise 2.1

---
+++ part2/Exercises 2.1.-2.5/src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exerciseTotal={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
+
+/*
+    
+*/
