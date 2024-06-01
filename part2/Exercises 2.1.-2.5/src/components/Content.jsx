cfbf7f6d0498900da2af3a8fac9f0dd37cfdc6f3
Phillip Radon <phillip.radon@yahoo.com>
2024-06-01 13:18:20 +0200

    final touches Exercises 2.5 my app did not look like the course example, edit the parts to not be a list anymore added a H1 and changed the coursenames to H2

--- part2/Exercises 2.1.-2.5/src/components/Content.jsx
+++ part2/Exercises 2.1.-2.5/src/components/Content.jsx
@@ -1,15 +1,14 @@
 const Content = ({ parts }) => {
-  const contentList = parts.map((item) => {
-    return (
-      <li key={item.id}>
-        {item.name} : {item.exercises}
-      </li>
-      //When creating a list in the UI from an array with JSX, you should add a key prop to each child and to any of its’ children. React uses the key prop create a relationship between the component and the DOM element. The library uses this relationship to determine whether or not the component should be re-rendered.
-    );
-  });
   return (
     <>
-      <ul>{contentList}</ul>
+      {parts.map((item) => {
+        return (
+          <p key={item.id}>
+            {item.name} : {item.exercises}
+          </p>
+          //When creating a list in the UI from an array with JSX, you should add a key prop to each child and to any of its’ children. React uses the key prop create a relationship between the component and the DOM element. The library uses this relationship to determine whether or not the component should be re-rendered.
+        );
+      })}
     </>
   );
 };
